27cf63578ad18543ac012ebc4428ca231808af3e
nikhil291 <123387548+nikhil291@users.noreply.github.com>
2023-07-22 11:07:10 +0530

    Add files via upload

---
+++ qsum.py
@@ -0,0 +1,8 @@
+import cmath
+a=int(input(" Enter a value of a : "))
+b=int(input(" Enter a value of b : "))
+c=int(input(" Enter a value of c : "))
+d=((b**2)-(4*a*c))
+sol1=(((-b)+cmath.sqrt(d))/(2*a))
+sol2=(((-b)-cmath.sqrt(d))/(2*a))
+print(f' The Quadratic Equation are ---\n {sol1}\n {sol2}')
