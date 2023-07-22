2262be3f1caa5dceff0d7584f972a8fa4af84e4f
nikhil291 <123387548+nikhil291@users.noreply.github.com>
2023-07-22 11:14:31 +0530

    Add files via upload

---
+++ natural.py
@@ -0,0 +1,10 @@
+num=int(input(" Enter a Number : "))
+i=1
+sum=0
+if(num<0):
+    print(" Enter a positive number ")
+else:
+    while(i<=num):
+        sum += i
+        i += 1
+print(f'The Sum of first {num} natural numbers is {sum} ')
