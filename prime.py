e8cab284bf1e4e71c9dca628a5f2c259c2a8d7cf
nikhil291 <123387548+nikhil291@users.noreply.github.com>
2023-07-22 11:18:32 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,13 @@
+n=int(input(" Enter a Number to check Number Wheather it is Prime or Not : "))
+flag=False
+if(n==1):
+    print(f'{n} is not a Prime Number')
+else:
+    for i in range(2,n):
+        if(n%i==0):
+            flag=True
+            break
+if(flag==False):
+    print(f'{n} is a prime number')
+else:
+      print(f'{n} is not a prime number')
